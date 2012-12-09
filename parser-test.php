511c56a609b890be4393ef4384f6e39ac8e052f2
Pau Rodriguez-Estivill <prodrigestivill@gmail.com>
2012-12-09 18:47:16 +0000

    Add parser params.

--- parser-test.php
+++ parser-test.php
@@ -1,8 +1,10 @@
 <?php
+$listfile = isset($argv[1])?$argv[1]:"list.txt";
+
 require("parser.inc.php");
 
 $ret = array();
-$total = import_data("list.txt", $ret);
+$total = import_data($listfile, $ret);
 print_r($ret);
 echo "Total: $total".PHP_EOL;
 ?>
